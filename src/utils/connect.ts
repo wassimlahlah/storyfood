cd47c9cad59e4dc78e6f9d27ec5f67940a3dbe57
wassimlahlah <wassimlahlah06@gmail.com>
2025-12-15 21:40:28 +0100

    Update Prisma packages

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,9 +1,12 @@
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+// تعريف نسخة واحدة من PrismaClient لتجنب تعدد الاتصالات أثناء التطوير
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// إذا نحن في وضع التطوير، خزّن النسخة على globalThis
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
